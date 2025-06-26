8086d52e252145f4e396e140c1db68c6fc12b63c
HifzanAli70938 <hifzanmoon@gmail.com>
2025-06-26 21:41:13 +0500

    title updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Devfolio | Portfolio Website using Next.js, Tailwind CSS, and Framer Motion",
-  description: "Devfolio is a portfolio website for developers to showcase their projects and skills.",
+  title: "Hifzan | Portfolio Website using Next.js, Tailwind CSS, and Framer Motion",
+  description: "Hifzan is a portfolio website  to showcase my projects and skills.",
 };
 
 export default function RootLayout({
